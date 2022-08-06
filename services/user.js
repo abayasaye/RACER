9a6cbc5b8c7aa4e2fa80ab881e739865568c65ce
abayasaye <abay400@gmail.com>
2022-08-06 23:44:45 +0300

    user

--- services/user.js
+++ services/user.js
@@ -1,4 +1,21 @@
-const BASIC_API = "https://motorsportstats.com/team/api-motorsport/summary/series/british-touring-car-championship"
+const BASIC_API = "https://my-json-server.typicode.com/Jeck99/fake-server/users"
+const usersPicturesArr= [
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fcelebs.infoseemedia.com%2Fwp-content%2Fuploads%2F2020%2F10%2FLewis-Hamilton.png&f=1&nofb=1",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fcelebs.infoseemedia.com%2Fwp-content%2Fuploads%2F2020%2F10%2FLewis-Hamilton.png&f=1&nofb=1",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fcelebs.infoseemedia.com%2Fwp-content%2Fuploads%2F2020%2F10%2FLewis-Hamilton.png&f=1&nofb=1",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fcelebs.infoseemedia.com%2Fwp-content%2Fuploads%2F2020%2F10%2FLewis-Hamilton.png&f=1&nofb=1",
+    "https://i.pinimg.com/736x/28/3a/b1/283ab1108ef8e379a2e555de019e1aee.jpg",
+    "https://us.123rf.com/450wm/fizkes/fizkes2010/fizkes201001384/157765614-profile-picture-of-smiling-indian-female-isolated-on-grey-studio-background-show-optimism-and-positi.jpg?ver=6",
+    "https://buffer.com/library/content/images/2022/03/amina.png",
+    "https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg",
+    "https://i.insider.com/59b6c4bfba785e36f932a317?width=600&format=jpeg&auto=webp",
+    "https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/profile-photos-4.jpg",
+    "https://media.istockphoto.com/photos/headshot-portrait-of-smiling-ethnic-businessman-in-office-picture-id1300512215?k=20&m=1300512215&s=612x612&w=0&h=enNAE_K3bhFRebyOAPFdJtX9ru7Fo4S9BZUZZQD3v20=",
+    "https://media.istockphoto.com/photos/portrait-of-successful-black-male-modern-day-student-holding-picture-id1311634222?k=20&m=1311634222&s=612x612&w=0&h=1a0XDWnZNPjk_5n7maZdzowaDfBcBohwoiZZF69qS9A=",
+    "https://chicagophotovideo.com/wp-content/uploads/2018/01/professional-headshots-for-linkedin-chicago-1024x1024.jpg",
+    "https://e9g2x6t2.rocketcdn.me/wp-content/uploads/2021/02/Website-Photo-11-1.jpg",
+    "https://i.pinimg.com/736x/42/57/dd/4257dd317ad8c667d62b47acd041788c.jpg"
+  ]
 async function printFromApi () {
 
     try{
@@ -11,12 +28,12 @@ async function printFromApi () {
             //         colector.innerHTML+=`<div class='col-md-3'>${result[i].DriverID}${result[i].FirstName}${result[i].LastName}<img class='w-25' src='${result[i].PhotoUrl}'/>${result[i][key]}${result[i].Number}</div> <br>`;
             //     }
             // }
-            // result.forEach(element => {
-            //     console.log(element);
-            //     for (const key in element) {
-            //         colector.innerHTML+=`${key} ${element.DriverID} ${element.FirstName}${element.PhotoUrl}`;
-            //     }
-            // });
+            result.forEach(element => {
+                console.log(element);
+                for (const key in element) {
+                    colector.innerHTML+=`${key}${element[key].picture=<img class='w-50' src = '${usersPicturesArr[index]}'/>}`;
+                }
+            });
 
 //             for(let i = 0 ; i<10 ; i++){
 //                 console.log(result[i]);
